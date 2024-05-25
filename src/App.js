8a6ec3f25f3a58b3a35f8bd6e9ec51b1fcf40418
Oloishorua <97743302+Kisotu@users.noreply.github.com>
2024-05-25 20:00:53 +0000

    imported a new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
+import Button from "./components/Button";
 
-export default function App() {
+export default function App(props) {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <p>this is a test</p>
+      <Button name="Hello" />
     </div>
   );
 }
